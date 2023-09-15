e6b630d83cd26f6de534720b6e53e26dcc521c97
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-09-15 06:10:56 -0500

    create sign up logic for component
    define a context to be used for the component that contains the target
    email address, display errro when invalid email is displayed, confirm
    message is displayed and user can navigate back to previous form.

--- src/features/confirmation/components/Confirmation.tsx
+++ src/features/confirmation/components/Confirmation.tsx
@@ -1,16 +1,33 @@
+import { useContext } from 'react'
 import checkmark from '@/src/assets/images/icon-success.svg'
+import { ActionContext } from '@/src/context/EmailContext'
 
-export const Confirmation = () => {
+interface ConfirmationProps {
+  email: string
+  reset: () => void
+}
+
+export const Confirmation = ({ email, reset }: ConfirmationProps) => {
+  const dispatch = useContext(ActionContext)
   return (
     <main>
       <img src={checkmark} alt=""></img>
       <h1>Thanks for subscribing!</h1>
       <p>
-        A confirmation email has been sent to ash@loremcompany.com. Please open
-        it and click the button inside to confirm your subscription
+        A confirmation email has been sent to <strong>{email}</strong>. Please
+        open it and click the button inside to confirm your subscription
       </p>
 
-      <button type="button">Dismiss message</button>
+      <button
+        type="button"
+        onClick={() => {
+          if (!dispatch) return
+          dispatch('')
+          reset()
+        }}
+      >
+        Dismiss message
+      </button>
     </main>
   )
 }
--- src/features/signup/components/sign-up/SignUp.tsx
+++ src/features/signup/components/sign-up/SignUp.tsx
@@ -1,10 +1,47 @@
-import { useState } from 'react'
+import { ActionContext } from '@/src/context/EmailContext'
+import { useContext, useState } from 'react'
 
-export const SignUp = () => {
+const emailRegex = /^[a-z0-9.]{1,64}@[a-z0-9.]{1,64}$/i
+
+interface SignUpProps {
+  submitEmail: React.Dispatch<boolean>
+}
+
+export const SignUp = ({ submitEmail }: SignUpProps) => {
+  const dispatch = useContext(ActionContext)
   const [text, setText] = useState('')
+  const [error, setError] = useState(false)
+
+  const confirmEmail = () => {
+    // if email empty, just don't do anything
+    if (!dispatch) return
+    if (text === '') return
+
+    // check for valid email address with regex
+    if (!emailRegex.test(text)) {
+      setError(true)
+      return
+    }
+
+    submitEmail(true)
+    setError(false)
+    dispatch(text)
+  }
+
   return (
-    <form>
-      <label htmlFor="signupEmail">Email address</label>
+    <form
+      noValidate
+      onSubmit={(e) => {
+        // does not reload the page
+        e.preventDefault()
+        confirmEmail()
+      }}
+    >
+      <div>
+        <label htmlFor="signupEmail">Email address</label>
+        {error ? <p aria-live="polite">Valid email required</p> : null}
+      </div>
+
       <input
         type="email"
         id="signupEmail"
